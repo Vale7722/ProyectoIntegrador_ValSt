66ef3da7513aea52ece57de6c7240aeb563b2005
Nicolas99414 <valeria.staudinger@gmail.com>
2022-04-20 12:55:26 -0300

    genera c experiencia

--- ProyectoIntegrador_ValSt/src/app/app.module.ts
+++ ProyectoIntegrador_ValSt/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
 import { SocialComponent } from './components/social/social.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 
 @NgModule({
   declarations: [
@@ -17,6 +18,7 @@ import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
     SocialComponent,
     BannerComponent,
     AcercaDeComponent,
+    ExperienciaComponent,
   ],
   imports: [
     BrowserModule,
