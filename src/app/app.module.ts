f3cb5d553f180aa78c8b2e83ee64cf9908b6f0aa
Nicolas99414 <valeria.staudinger@gmail.com>
2022-05-11 13:04:47 -0300

    se suma la barra de progreso

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { SocialComponent } from './components/social/social.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { HysComponent } from './components/hys/hys.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
 
 @NgModule({
   declarations: [
@@ -19,12 +21,15 @@ import { ExperienciaComponent } from './components/experiencia/experiencia.compo
     BannerComponent,
     AcercaDeComponent,
     ExperienciaComponent,
+    HysComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
   ],
   providers: [],
   bootstrap: [AppComponent]
+
 })
 export class AppModule { }
